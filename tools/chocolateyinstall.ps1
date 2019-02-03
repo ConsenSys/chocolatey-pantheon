4197db506745decae3b5d0211ee5ccb650459082
Rob Dawson <robert@rojotek.com>
2019-02-03 14:03:06 +1000

    Upgraded pantheon to 0.9.1, improved dependencies and environment setup.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,18 +2,27 @@
 # https://github.com/PegaSysEng/homebrew-pantheon/blob/master/pantheon.rb
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$chocoJavaDir = "c:\Program Files\OpenJDK\jdk-11.0.2"
 $packageArgs = @{
   PackageName = 'pantheon'
-  Url64bit = 'https://consensys.bintray.com/pegasys-repo/pantheon-0.8.3.zip'
-  Checksum64 = '48f5fdf7e1f8a7e460ecb86cbf3b8ac45c188af9d8b973e2d9ceab8d57b82a25'
+  Url64bit = 'https://consensys.bintray.com/pegasys-repo/pantheon-0.9.1.zip'
+  Checksum64 = '21adc39f04b3fb16d66d2e254a9915c2201234af60a79925ad4f980f2de3d052'
   ChecksumType64 = 'sha256'
   UnzipLocation= $toolsDir
 }
+$javaHomeArgs = @{
+  VariableType = 'Machine'
+  VariableValue = $chocoJavaDir
+  VariableName = "JAVA_HOME"
+}
 Install-ChocolateyZipPackage  @packageArgs
+
+Install-ChocolateyEnvironmentVariable @javaHomeArgs
+Update-SessionEnvironment
 
 $binArgs = @{
   Name = 'pantheon'
-  Path = "$toolsDir\pantheon-0.8.3\bin\pantheon.bat"
+  Path = "$toolsDir\pantheon-0.9.1\bin\pantheon.bat"
 }
 
 Install-BinFile @binArgs
