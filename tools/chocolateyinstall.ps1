46ff4cd4da0f1a0fe8488915e26a770b20b4e5ae
Rob Dawson <robert@rojotek.com>
2019-02-28 17:54:19 +0000

    Updated to version 1.0.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,27 +2,20 @@
 # https://github.com/PegaSysEng/homebrew-pantheon/blob/master/pantheon.rb
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$chocoJavaDir = "c:\Program Files\OpenJDK\jdk-11.0.2"
 $packageArgs = @{
   PackageName = 'pantheon'
-  Url64bit = 'https://consensys.bintray.com/pegasys-repo/pantheon-0.9.1.zip'
-  Checksum64 = '21adc39f04b3fb16d66d2e254a9915c2201234af60a79925ad4f980f2de3d052'
+  Url64bit = 'https://consensys.bintray.com/pegasys-repo/pantheon-1.0.0.zip'
+  Checksum64 = '6e53e75a26c7b6db50aea7d6deef3bac1ff20b1da8851b847fddf6350261b161'
   ChecksumType64 = 'sha256'
   UnzipLocation= $toolsDir
 }
-$javaHomeArgs = @{
-  VariableType = 'Machine'
-  VariableValue = $chocoJavaDir
-  VariableName = "JAVA_HOME"
-}
 Install-ChocolateyZipPackage  @packageArgs
 
-Install-ChocolateyEnvironmentVariable @javaHomeArgs
 Update-SessionEnvironment
 
 $binArgs = @{
   Name = 'pantheon'
-  Path = "$toolsDir\pantheon-0.9.1\bin\pantheon.bat"
+  Path = "$toolsDir\pantheon-1.0.0\bin\pantheon.bat"
 }
 
 Install-BinFile @binArgs
