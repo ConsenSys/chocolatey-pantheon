4e87c1a024ddb490ec9d71656e58a21d69f9fb59
Rob Dawson <robert@rojotek.com>
2019-03-08 10:02:40 +0100

    1.0.1 release.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   PackageName = 'pantheon'
-  Url64bit = 'https://consensys.bintray.com/pegasys-repo/pantheon-1.0.0.zip'
-  Checksum64 = '6e53e75a26c7b6db50aea7d6deef3bac1ff20b1da8851b847fddf6350261b161'
+  Url64bit = 'https://consensys.bintray.com/pegasys-repo/pantheon-1.0.1.zip'
+  Checksum64 = 'acb015f6467f623fec96de876b274eaccf08951c298e2c5eaf15d45784e3dbf8'
   ChecksumType64 = 'sha256'
   UnzipLocation= $toolsDir
 }
@@ -15,7 +15,7 @@ Update-SessionEnvironment
 
 $binArgs = @{
   Name = 'pantheon'
-  Path = "$toolsDir\pantheon-1.0.0\bin\pantheon.bat"
+  Path = "$toolsDir\pantheon-1.0.1\bin\pantheon.bat"
 }
 
 Install-BinFile @binArgs
